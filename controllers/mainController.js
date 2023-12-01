0bb70fdc380acd66aa7efbed3aa2234de65bb072
gracecatk <gracie.kiker@gmail.com>
2023-11-30 19:51:30 -0600

    added correct api key to mainControllers.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -9,7 +9,7 @@
   // Handle adding a new song
 //};
 const fetchMyData = async () => {
-  const response = await fetch('https://api.github.com/users');
+  const response = await fetch('https://accounts.spotify.com/authorize?client_id=df91343a82f94f9180f4ca27942cdb95&response_type=code&redirext_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state');
   const data = await response.json();
   const loginNames = data.map(item => item.login);
 
