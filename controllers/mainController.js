1031e0ba57f8726a3eb5280c58c35d6b845f0093
gracecatk <gracie.kiker@gmail.com>
2023-11-30 19:46:57 -0600

    added mainController.js code

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,10 +1,23 @@
 // controllers/mainController.js
-const db = require('../models/index');
+//const db = require('../models/index');
 
-exports.getHome = (req, res) => {
+//exports.getHome = (req, res) => {
   // Render home page with songs
-};
+//};
 
-exports.addSong = (req, res) => {
+//exports.addSong = (req, res) => {
   // Handle adding a new song
-};
+//};
+const fetchMyData = async () => {
+  const response = await fetch('https://api.github.com/users');
+  const data = await response.json();
+  const loginNames = data.map(item => item.login);
+
+  // After fetching, compile and render the template
+  const templateSource = document.getElementById('loginNames-template').innerHTML;
+  const template = Handlebars.compile(templateSource);
+  const html = template({ loginNames });
+  document.getElementById('template-container').innerHTML = html;
+}
+
+fetchMyData();
