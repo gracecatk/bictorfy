ed6ab6f83bae7bd8cb13faa2045ecca085df9c25
unknown <armore_16@hotmail.com>
2023-12-05 18:54:34 -0600

    created login route

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const apiRoutes = require('./api')
 const homeRoutes = require('./homeRoutes.js')
+const loginRoutes = require('./loginController.js')
 router.use('/', homeRoutes)
 router.use('/api',apiRoutes)
+router.use('/login',loginRoutes)
 module.exports = router 
